e80bf691044cfca889cba95464ef32a059865fd8
miguelcarmo5 <miguelcarmo5@hotmail.com>
2025-09-08 10:31:48 -0300

    feat:cria pedidos de forma aleatoria

--- cardapio.h
+++ cardapio.h
@@ -3,29 +3,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
+#include <time.h>
 #include "pilha.h"
 #define valorBase 1.5
 #define tamanho 10
 
-typedef struct {
+typedef struct {  // Criação da struct tp_hamburger
     char nome[20];
     int id;
     float valor;
     tp_pilha ingrediente;
 }tp_hamburger;
 
-void imprimecardapio(tp_hamburger *p){
+void pedidos(tp_hamburger *cardapio, int dia){  // Gera pedidos de forma aleatoria com base no dia(fase) do jogo
+    for(int i=0; i<dia; i++){
+        int valor = (rand() % 10);
+        printf("%s\n", cardapio[valor].nome);
+    }
+}
+
+void imprimecardapio(tp_hamburger *p){  // Imprime o nome, preço e a pilha de ingredientes de cada hamburger
     for(int i=0; i<tamanho; i++){
-        printf("\n%s\n%d R$%.2f\n", p[i].nome, p[i].id, p[i].valor);
+        printf("\n%s R$%.2f\n", p[i].nome, p[i].valor);
         imprimepilha(p[i].ingrediente);
     }
 }
 
-void inicializacardapio(tp_hamburger *cardapio){
+void inicializacardapio(tp_hamburger *cardapio){  // Set de todos os dados de todos os hamburgeres
     
     strcpy(cardapio[0].nome, "Bit and Bacon");
-    cardapio[0].id = 145;
-    cardapio[0].valor = valorBase*20;
+    cardapio[0].id = 150;
+    cardapio[0].valor = valorBase*35;
     inicializapilha(&cardapio[0].ingrediente);
     push(&cardapio[0].ingrediente, "Pao");
     push(&cardapio[0].ingrediente, "Queijo");
@@ -34,8 +42,8 @@ void inicializacardapio(tp_hamburger *cardapio){
     push(&cardapio[0].ingrediente, "Pao");
 
     strcpy(cardapio[1].nome, "Duck Cheese");
-    cardapio[1].id = 150;
-    cardapio[1].valor = valorBase*30;
+    cardapio[1].id = 155;
+    cardapio[1].valor = valorBase*25;
     inicializapilha(&cardapio[1].ingrediente);
     push(&cardapio[1].ingrediente, "Pao");
     push(&cardapio[1].ingrediente, "Queijo");
@@ -43,7 +51,7 @@ void inicializacardapio(tp_hamburger *cardapio){
     push(&cardapio[1].ingrediente, "Alface");
     push(&cardapio[1].ingrediente, "Pao");
  
-    strcpy(cardapio[2].nome, "Quackteirão");
+    strcpy(cardapio[2].nome, "Quackteirao");
     cardapio[2].id = 160;
     cardapio[2].valor = valorBase*40;
     inicializapilha(&cardapio[2].ingrediente);
@@ -55,8 +63,8 @@ void inicializacardapio(tp_hamburger *cardapio){
     push(&cardapio[2].ingrediente, "Pao");
 
     strcpy(cardapio[3].nome, "Big Pato");
-    cardapio[3].id = 160;
-    cardapio[3].valor = valorBase*50;
+    cardapio[3].id = 170;
+    cardapio[3].valor = valorBase*60;
     inicializapilha(&cardapio[3].ingrediente);
     push(&cardapio[3].ingrediente, "Pao");
     push(&cardapio[3].ingrediente, "Alface");
@@ -69,7 +77,7 @@ void inicializacardapio(tp_hamburger *cardapio){
 
     strcpy(cardapio[4].nome, "Vegan Pato");
     cardapio[4].id = 165;
-    cardapio[4].valor = valorBase*50;
+    cardapio[4].valor = valorBase*30;
     inicializapilha(&cardapio[4].ingrediente);
     push(&cardapio[4].ingrediente, "Pao");
     push(&cardapio[4].ingrediente, "Tomate");
@@ -78,8 +86,8 @@ void inicializacardapio(tp_hamburger *cardapio){
     push(&cardapio[4].ingrediente, "Pao");
 
     strcpy(cardapio[5].nome, "Chicken Burguer");
-    cardapio[5].id = 170;
-    cardapio[5].valor = valorBase*50;
+    cardapio[5].id = 175;
+    cardapio[5].valor = valorBase*40;
     inicializapilha(&cardapio[5].ingrediente);
     push(&cardapio[5].ingrediente, "Pao");
     push(&cardapio[5].ingrediente, "Frango Empanado");
@@ -89,9 +97,9 @@ void inicializacardapio(tp_hamburger *cardapio){
     push(&cardapio[5].ingrediente, "Maionese Temperada");
     push(&cardapio[5].ingrediente, "Pao");
 
-    strcpy(cardapio[6].nome, "Chedão");
-    cardapio[6].id = 175;
-    cardapio[6].valor = valorBase*50;
+    strcpy(cardapio[6].nome, "Chedao");
+    cardapio[6].id = 180;
+    cardapio[6].valor = valorBase*45;
     inicializapilha(&cardapio[6].ingrediente);
     push(&cardapio[6].ingrediente, "Pao");
     push(&cardapio[6].ingrediente, "Carne");
@@ -102,7 +110,7 @@ void inicializacardapio(tp_hamburger *cardapio){
     push(&cardapio[6].ingrediente, "Pao");
 
     strcpy(cardapio[7].nome, "Bacon 2.0");
-    cardapio[7].id = 180;
+    cardapio[7].id = 190;
     cardapio[7].valor = valorBase*50;
     inicializapilha(&cardapio[7].ingrediente);
     push(&cardapio[7].ingrediente, "Pao");
@@ -115,7 +123,7 @@ void inicializacardapio(tp_hamburger *cardapio){
 
     strcpy(cardapio[8].nome, "Pato Egg");
     cardapio[8].id = 185;
-    cardapio[8].valor = valorBase*50;
+    cardapio[8].valor = valorBase*35;
     inicializapilha(&cardapio[8].ingrediente);
     push(&cardapio[8].ingrediente, "Pao");
     push(&cardapio[8].ingrediente, "Maionese Temperada");
@@ -124,8 +132,8 @@ void inicializacardapio(tp_hamburger *cardapio){
     push(&cardapio[8].ingrediente, "Pao");
 
     strcpy(cardapio[9].nome, "Pa-Tudo");
-    cardapio[9].id = 190;
-    cardapio[9].valor = valorBase*50;
+    cardapio[9].id = 195;
+    cardapio[9].valor = valorBase*80;
     inicializapilha(&cardapio[9].ingrediente);
     push(&cardapio[9].ingrediente, "Pao");
     push(&cardapio[9].ingrediente, "Carne");
--- main.c
+++ main.c
@@ -10,14 +10,25 @@
 
 
 int main(){
+    srand(time(NULL));
     setlocale(LC_ALL, "Portuguese");
+
+
     tp_hamburger cardapio[10];
     inicializacardapio(cardapio);
     imprimecardapio(cardapio);
+    int dia = 1;
+    printf("\n\n\nGerando pedidos aleatórios com base no dia:\n");
+    for(int i=0; i<5; i++){
+        printf("\n%d dia\n", dia);
+        pedidos(cardapio, dia);
+        dia++;
+    }
+    /*
     tp_fila fila_pedidos;                
     inicializaFila(&fila_pedidos);
     tp_pedido p = criar_pedido(145, 2);
     insereFila(&fila_pedidos, p);
-
+    */
     return 0;
 }
